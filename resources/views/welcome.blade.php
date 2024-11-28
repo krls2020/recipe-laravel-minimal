b992992f6d7ba26e50affa6cb6f91939b022a7c3
krls2020 <mail@karelhemza.cz>
2024-11-28 09:14:37 +0100

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,13 +75,10 @@
                                                 ✉️ Welcome email has been sent
                                             </p>
                                             @if(config('app.mailpit_zerops_subdomain'))
-                                                <a href="https://{{ config('app.mailpit_zerops_subdomain') }}"
+                                                <a href="{{ config('app.mailpit_zerops_subdomain') }}"
                                                    target="_blank"
-                                                   class="mt-2 inline-flex items-center text-sm text-blue-600 dark:text-blue-300 hover:underline">
+                                                   class="mt-4 inline-flex items-center text-sm text-blue-600 dark:text-blue-300 hover:underline">
                                                     👉 View sent email in Mailpit
-                                                    <svg class="ml-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
-                                                    </svg>
                                                 </a>
                                             @endif
                                         </div>
